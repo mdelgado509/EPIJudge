796f7365036ea28fa900b5422f51d3622d849dc1
mdelgado509 <mdelgado509@gmail.com>
2021-06-22 17:43:59 -0400

    Solve swap bits algo

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -1,9 +1,21 @@
+"""Takes a 64-bit integer and swaps the bits at indices i and j"""
 from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    """Takes a 64-bit integer and swaps the bits at indices i and j"""
+    # Extract the i-th annd j-th bit by bitwise right shifting
+    # with a value of i and j repectively and return that value
+    # by bitwise & with 1
+    # Compare the values to check if they are different
+    if (x >> i) & 1 != (x >> j) & 1:
+        # set a bit mask by setting bits at indices i and j through left
+        # shifting 1 by the respective values and taking the bitwise
+        # OR result
+        bit_mask = (1 << i) | (1 << j)
+        # XOR x and bit_mask to switch bits at indices i and j
+        x ^= bit_mask
+    return x
 
 
 if __name__ == '__main__':
