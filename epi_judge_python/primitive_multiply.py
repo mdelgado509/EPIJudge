e50ef6ceb16346e47efe2ef6b9b5ce5d865f8078
mdelgado509 <mdelgado509@gmail.com>
2021-06-28 15:13:28 -0400

    Solve multiply without arithmetic operators

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,32 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Next approach from book solution
+    # for each set bit of x it adds 2^k * y to the result
+    # k is the k-th bit that is set in x
+    # time complexity O(n^2)
+    def add(a, b):
+        return a if b == 0 else add(a ^ b, (a & b) << 1)
+
+    running_sum = 0
+    while x:  # Examines each bit of x.
+        # if last bit is 1
+        if x & 1:
+            # add y to running sum
+            running_sum = add(running_sum, y)
+        # right shift x to move to next bit and left shift y to increase
+        # bit width by 1
+        x, y = x >> 1, y << 1
+    return running_sum
+    #
+    # brute force takes ~ 3 ms
+    # O(2^n) time complexity where n is the number of bits in the input
+    # still doesn't solve for how to multiply without add instructions
+    result = 0
+    while y:
+        result = result + x
+        y = y - 1
+    return result
 
 
 if __name__ == '__main__':
