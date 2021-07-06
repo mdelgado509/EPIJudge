f46658e1c12e6b2e85f590a256301e9d304398e8
mdelgado509 <mdelgado509@gmail.com>
2021-07-06 13:36:51 -0400

    Add solutions to power x y and primitive divide

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,44 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # this next approach we use recursion and divide and conquer strategy
+    # this time complexity run time is O(log(n))
+    #
+    if y == 0: return 1 # power of 0 result is 1
+    if y == 1: return x # power of 1 result is x
+    # define temp result by recursively calling power with y/2
+    temp = power(x, int(y / 2))
+
+    if y % 2 == 0: # if y is even
+        return temp * temp
+    else: # if y is odd
+        if y > 0: return x * temp * temp
+        else: return (temp * temp) / x # if y is odd and negative
+
+
+    # a brute force approach is to multiply x y times
+    # this achieves linear time complextiy and we can do better
+    # by exploiting the properties of exponentiation
+    #
+    # if y is even then x^y is just (x^(y/2))^2
+    # if y is odd then we just do x * (x^(y/2))^2
+    #
+    # We can examine each bit of y
+    # if the LSB is 1 then we multiply the result by x
+    # if it's even or odd x = x * x = x^2 etc... and y is right shifted 1 bit
+    #
+    # if y is odd we just set power = -power and x = 1/x
+    #
+    # this algo achieves O(n) time complexity where n is at most the index
+    # of the MSB of y
+    # result, power = 1.0, y
+    # if y < 0: # if y is negative
+    #     power, x = -power, 1.0 / x
+    # while power: # examine each bit of power
+    #     if power & 1:
+    #         result = result * x
+    #     x, power = x * x, power >> 1
+    # return result
 
 
 if __name__ == '__main__':
--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # compute quotient without arithmetical operators
+    # use only addition subtraction and shifting operators
+    #
+    # book suggests to subtract 2^k * y from x where 2^k * y <= x
+    # 2^k * y = y << k       and then add (1 << k) to the quotient
+    # this gets more done with each round of subtraction
+    #
+    # This is O(n) time complexity where n is x/y in number of bits
+    #
+    # first define the result and the power k int
+    # we set to 32 because we assume max bit size input is 64
+    result, k = 0, 32
+    # this is the 2ky variable
+    y_pow_k = y << k
+    while x >= y: # repeat until y > x
+        while y_pow_k > x: # loop until x > y_pow_k
+            y_pow_k >> 1 # right shift y_pow_k by 1
+            k -= 1 # decrement k power
+        result += 1 << k # add (1 << k) to the result
+        x -= y_pow_k # subtract y_pow_k from x
+    return result
 
 
 if __name__ == '__main__':
