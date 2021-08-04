5e3d8abc162ea422548028da97f1359e07e7fe60
mdelgado509 <mdelgado509@gmail.com>
2021-08-03 21:50:10 -0400

    Add solutions to 5.1 and 5.2

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,9 +9,106 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    # In this approach we use 4 subarrays
+    # 1. element is less than pivot    : A[equal]  < pivot : A[:smaller]
+    # 2. element is equal to pivot     : A[equal] == pivot : A[smaller:equal]
+    # 3. element is unclassified       : A[equal]  ? pivot : A[equal:larger]
+    # 4. element is greater than pivot : A[equal]  > pivot : A[larger:]
+    # O(n) time O(1) space
+    # Approach 4:
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0, 0, len(A)
+    while equal < larger: # while there are unclassified elements
+        # A[equal] are the unclassified elements
+        if A[equal] < pivot:
+            # switch upperbound of smaller subarray with current
+            # unclassified element
+            # increment both pointers by 1
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            # increase the unclassified pointer to 1
+            # increases unclassified subarray by 1
+            equal += 1
+        else: # A[equal] > pivot
+            # decrement larger subarray upperbound pointer by 1
+            # switch lower bound of larger subarray with
+            # current unclassified element
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
 
+
+    # In this approach we use two pointers to keep track
+    # of the next available position to place elements
+    # smaller than the pivot in the front. and elements
+    # larger than the pivot in the back of the array
+    # this has O(n) and O(1) time and space complexity, respectively
+    # Approach 3:
+    pivot = A[pivot_index]
+    # First pass: group elements smaller than pivot
+    # at the front of the array
+    front_pointer = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[front_pointer] = A[front_pointer], A[i]
+            front_pointer += 1
+    # Second pass: group elements larger than pivot
+    # at the back of the array
+    back_pointer = len(A) - 1
+    for i in reversed(range(len(A))):
+        if A[i] > pivot:
+            A[i], A[back_pointer] = A[back_pointer], A[i]
+            back_pointer -= 1
+
+
+    # this first approach improves a O(n) space complexity
+    # to O(1) by using the array itself to sort, but the
+    # time complexity is O(n^2) two nested loops
+    # Approach 2:
+    pivot = A[pivot_index]
+    # First pass: start from front and move elements smaller than
+    # pivot to the front of the array
+    for i in range(len(A)):
+        # Look for element smaller than pivot to place at index i
+        for j in range(i + 1, len(A)):
+            if A[j] < pivot:
+                A[i], A[j] = A[j], A[i]
+                break # switch elements and move i to the next index
+    # Second pass: from the back group elements larger than pivot at
+    # the back of the array
+    for i in reversed(range(len(A))):
+        # Look for a larger element. Stop when we reach an element larger
+        # Than pivot and switch with element at i (from te back)
+        for j in reversed(range(i)):
+            if A[j] > pivot:
+                A[i], A[j] = A[j], A[i]
+                break
+
+    # Approach 2: Create three subarrays
+    # O(n) time O(n) space
+    pivot = A[pivot_index]
+    smaller = []
+    equal = []
+    larger = []
+
+    for val in A:
+        if val < pivot:
+            smaller.append(val)
+        elif val == pivot:
+            equal.append(val)
+        else: # val >
+            larger.append(val)
+    for i in range(len(A)):
+        if smaller:
+            A[i] = smaller.pop()
+        elif equal:
+            A[i] = equal.pop()
+        else:
+            A[i] = larger.pop()
+
+    # Approach 1: Built in sort method
+    # O(nlog(n))
+    A.sort()
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,26 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # This is modeled after the grade-school addition algorithm
+    # O(n) time complexity and O(1) space (slightly altered from book)
+
+    # Start from least significant digit (in our case the back
+    # of the list) and add 1
+    A[-1] += 1
+
+    # Then iterate from last to the second digit (don't include the first)
+    for i in reversed(range(1, len(A))):
+        # If A[i] is 10 then set to 0 and increment A[i - 1]
+        if A[i] == 10:
+            A[i] = 0
+            A[i - 1] += 1
+
+    # if the A[0] digit is 10 set to 1 and append a 0
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+
+    return A
 
 
 if __name__ == '__main__':
